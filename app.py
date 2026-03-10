68b459c670382d9b6ec46e9f6b62fdb03f88d9ff
tassnimelleuch <tasnim.eleuch@enis.tn>
2026-03-10 04:08:37 +0100

    added left navbar

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from config import Config
 def create_app():
     app= Flask(__name__)
     app.config.from_object(Config)
+
     from auth import auth_bp
     app.register_blueprint(auth_bp)
 
     from flask import Blueprint, render_template
     main_bp = Blueprint('main', __name__)
+
     @main_bp.route('/')
     def index():
         return redirect(url_for('auth.login'))
@@ -19,8 +21,18 @@ def create_app():
             return redirect(url_for('auth.login'))
         return render_template('dashboard_placeholder.html', username=session['username'],role=session['role'])
     app.register_blueprint(main_bp)
-    return app
 
+    #to display the number of pending users in the navbar badge
+
+    @app.context_processor
+    def inject_globals():
+        from models import get_pending_count
+        count = 0
+        if session.get('role') == 'admin':
+            count = get_pending_count()
+        return {'pending_count': count}
+
+    return app
 if __name__ == '__main__':
     app = create_app()
     app.run(debug=True)
